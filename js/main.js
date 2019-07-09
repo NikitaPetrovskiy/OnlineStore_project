1afbf1d15817b3aeda923d83e91d24d9c32b46e8
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-07-09 19:33:39 +0300

    Added and connected jquery ui custom files. Created slider-box

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-$(function () {
     "use strict";
 
     $('.title_box').on('click', function () {
@@ -7,6 +6,20 @@ $(function () {
         $(this).next('.list_link').toggleClass('open');
     });
 
+    $('#slider_price').slider({
+        max: 1000,
+        min: 0,
+        range: true,
+        values: [190, 728],
+        slide: function (event, ui) {
+         $('input[name="minPrice"]').val('$' + ui.values[0]);
+         $('input[name="maxPrice"]').val('$' + ui.values[1]);
+        }
+    });
 
-})
+    $('input[name="minPrice"]').val('$' +
+        $('#slider_price').slider('values', 0) );
+    $('input[name="maxPrice"]').val('$' +
+        $('#slider_price').slider('values', 1) );
+
 });
